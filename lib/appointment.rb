2afb30462b0e83ac1ed6c600345eff03d4635d34
Devin Wilson <craigvin84@yahoo.com>
2020-11-17 21:00:30 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,27 @@
-class Appointment
+class Patient
 
-  attr_accessor :date, :patient, :doctor
+  attr_accessor :name, :appointment, :doctor
   @@all = []
 
-  def initialize(patient, date, doctor)
-    @patient = patient
-    @date = date
-    @doctor = doctor
+  def initialize(name)
+    @name = name
     @@all << self
   end
 
   def self.all
     @@all
   end
-end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
+
+end 
