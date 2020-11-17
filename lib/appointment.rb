f340d6490721abd5d365ed443c17c4577af22821
Devin Wilson <craigvin84@yahoo.com>
2020-11-17 20:54:39 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,23 +3,15 @@ class Appointment
 
   @@all = []
 
-  def initialize(patient, doctor, date)
+  def initialize(date, patient, doctor)
+    @date = date
     @patient = patient
     @doctor = doctor
-    @date = date
+
     @@all << self
   end
 
   def self.all
     @@all
   end
-
-  def patient
-    @patient
-  end
-
-  def doctor
-    @doctor
-  end
-
-end    
+end
