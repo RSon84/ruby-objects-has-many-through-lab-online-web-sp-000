f6932664333e63d550c127d287215bced82d8ae5
Devin Wilson <craigvin84@yahoo.com>
2020-11-17 20:01:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,7 +13,7 @@ class Doctor
   end
 
   def patients
-    appointments.map {|appointment| appointment.doctor.name}
+    appointments.map {|appointment| appointment.patient}
   end
 
   def self.all
