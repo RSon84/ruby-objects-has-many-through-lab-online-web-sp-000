0001224f0f1f3217d6256efa9f5fe51145d5b47f
Devin Wilson <craigvin84@yahoo.com>
2020-11-17 20:57:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,27 +1,29 @@
 class Doctor
-  @@all=[]
+  attr_accessor :name, :appointment, :patient
+
+  @@all = Array.new
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
   def self.all
     @@all
   end
 
-  attr_accessor :name, :appointments, :patient
-
-  def initialize(name)
-    @name = name
-    @appointments = []
-    @@all << self
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
   end
 
-  def new_appointment(date, patient)
-     appointment = Appointment.new(patient, date, self)
-     @appointments << appointment
-     appointment
+  def appointments
+    Appointment.all.select do |apt|
+      apt.doctor == self
+      #binding.pry
+    end
   end
 
   def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
+    appointments.collect { |apt| apt.patient }
   end
 end
